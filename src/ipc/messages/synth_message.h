875315d3d8a0c619c8217b33e8bc7d251eee62ef
Kostya Farber <kostya.farber@gmail.com>
2024-11-18 08:19:27 +0000

    add basic queue structure

--- src/ipc/messages/synth_message.h
+++ src/ipc/messages/synth_message.h
@@ -8,10 +8,9 @@ class SynthMessage
   SynthCommand m_message;
 
   union PayloadData {
-    struct {
-      float frequency;
-    } freq;
-    
+    float frequency;
+    float volume;
+    float duty_cycle;
     std::uint8_t padding[16];
   } data;
 };
---
+++ src/ipc/queue.h
@@ -0,0 +1,21 @@
+#include <cstdint>
+namespace ipc {
+template <typename Message>
+class Queue
+{
+  Queue() {};
+  ~Queue() {};
+
+  Queue(const Queue &) = delete;
+  const Queue &operator=(const Queue &) = delete;
+
+  // want to delete copy
+  // not sure when we'd ever wantto move this thing?
+
+  void try_send(Message m) noexcept;
+  void try_receive(Message &m) noexcept;
+
+private:
+  uint32_t m_sequence{0};
+};
+} // namespace ipc
